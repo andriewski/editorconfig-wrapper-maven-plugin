42bb74a1b60ddd73801e30ec9160a7394bdb8eab
Mark <plambiroo@gmail.com>
2022-04-26 22:01:29 +0300

    Implementing plugin functionality

--- src/main/java/by/mark/spotless/wrapper/IdeaWorkSpaceXmlSettings.java
+++ src/main/java/by/mark/spotless/wrapper/IdeaWorkSpaceXmlSettings.java
@@ -2,16 +2,19 @@ package by.mark.spotless.wrapper;
 
 final class IdeaWorkSpaceXmlSettings {
 
+    static final String OPTIMIZE_ON_SAVE_OPTIONS = "OptimizeOnSaveOptions";
+    static final String FORMAT_ON_SAVE_OPTIONS = "FormatOnSaveOptions";
+
     // @formatter:off
     static final String OPTIMIZE_IMPORTS =
             "<component name=\"OptimizeOnSaveOptions\">\n" +
                 "<option name=\"myRunOnSave\" value=\"true\" />\n" +
-            "</component>";
+            "</component>\n";
 
     static final String REFORMAT_CODE =
             "<component name=\"FormatOnSaveOptions\">\n" +
                 "<option name=\"myRunOnSave\" value=\"true\" />\n" +
-            "</component>";
+            "</component>\n";
     // @formatter:on
 
     private IdeaWorkSpaceXmlSettings() {
